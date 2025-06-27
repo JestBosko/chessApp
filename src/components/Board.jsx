cc22489e756da1d15de9b8b2088766409f844c4a
Michał Kasprzak <m.kasprzak8708@gmail.com>
2025-06-27 13:22:26 +0200

    Board.jsx added and styled in Board.css(just a bit cleaner code)

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import './Board.css';
 
-const verticalAxis = [8,7,6,5,4,3,2,1]; // reversed for chess notation
-const horizontalAxis = ['a','b','c','d','e','f','g','h'];
+const verticalAxis = [8, 7, 6, 5, 4, 3, 2, 1];
+const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 function Board() {
-    let squares = [];
-    for (let i = 0; i < verticalAxis.length; i++) {
-        for (let j = 0; j < horizontalAxis.length; j++) {
-            let squareColor = (i + j) % 2 === 0 ? 'white' : 'black';
-            squares.push(
-                <div key={`${i}-${j}`} className={`square ${squareColor}`}>
-                    <span className="coordinate">{horizontalAxis[j]}{verticalAxis[i]}</span>
-                </div>
-            );
-        }
-    }
-    return <div id='board'>{squares}</div>
+    return (
+        <div id="board">
+            {verticalAxis.map((v, i) =>
+                horizontalAxis.map((h, j) => {
+                    const squareColor = (i + j) % 2 === 0 ? 'white' : 'black';
+                    return (
+                        <div key={`${i}-${j}`} className={`square ${squareColor}`}>
+                            <span className="coordinate">{h}{v}</span>
+                        </div>
+                    );
+                })
+            )}
+        </div>
+    );
 }
 
 export default Board;
